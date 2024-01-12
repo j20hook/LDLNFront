29100469283836f1aae035093067daaa9d3b9258
Jose Maria Garcia <jose.garcia.unamuno@gmail.com>
2024-01-12 17:42:02 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
+// Importamos el servidor de Angular
 import { NgModule } from '@angular/core';
+
+// Importamos el browser
 import { BrowserModule } from '@angular/platform-browser';
 
+// Importamos el Routing module para hacer las rutas
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+// Importamos el generador de iconos
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Importamos los componentes
 import { AppComponent } from './app.component';
+
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    CommonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
