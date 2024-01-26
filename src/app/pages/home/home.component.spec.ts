b6434b1e417b76f0b5ad842d7495ffb8d8fe4df3
Jose Maria Garcia <jose.garcia.unamuno@gmail.com>
2024-01-26 18:45:21 +0100

    Actualizacion de proyecto

---
+++ src/app/pages/home/home.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeComponent } from './home.component';
+
+import { Component } from '@angular/core';
+
+describe('HomeComponent', () => {
+    let component: HomeComponent;
+    let fixture: ComponentFixture<HomeComponent>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [HomeComponent],
+        });
+        fixture = TestBed.createComponent(HomeComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
