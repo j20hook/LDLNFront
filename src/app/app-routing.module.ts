f9d7acf78556da5a643436e4eb442d9ff8124f7a
Eduardi <eruizgarcia@safareyes.es>
2024-01-30 18:04:25 +0100

    actualizacion de estilo pagina video

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,6 @@ const routes: Routes = [
   {path:'', redirectTo:'/apollo', pathMatch:'full'},
   {path:'registro', component : CrearUsuarioPruebaComponent},
   {path:'error404', component : ErrorcuatrocientoscuatroComponent},
-  {path:'apollo/video', component : VideoComponent},
   {path:'apollo/video/:id', component : VideoComponent},
   {path:'apollo/video/subir', component : SubirVideoComponent},
   {path:'login' , component : LoginUsuarioComponent}
--- src/app/components/video/video.component.ts
+++ src/app/components/video/video.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {GeneralService} from "../../services/general.service";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {Video} from "../../models/Video";
 
 @Component({
@@ -11,15 +11,17 @@ import {Video} from "../../models/Video";
 export class VideoComponent implements OnInit{
 
   video = new Video();
+  id_video : string | null;
 
-  constructor(private service: GeneralService, public router: Router) {
+  constructor(private service: GeneralService, public router: Router, public _route:ActivatedRoute) {
+
+    this.id_video = this._route.snapshot.paramMap.get('id');
+
   }
 
   ngOnInit() {
 
-    localStorage.setItem('id_video', '2')
-
-      this.service.getVideoPorId(Number(localStorage.getItem('id_video'))).subscribe((data:any)=>{
+      this.service.getVideoPorId(Number(this.id_video)).subscribe((data:any)=>{
 
         this.video.id = data['id'];
         this.video.titulo = data['titulo'];
@@ -29,7 +31,6 @@ export class VideoComponent implements OnInit{
         this.video.fecha_publicacion = data['fechaPublicacion'];
         this.video.fecha_creacion = data['fechaCreacion'];
         this.video.canal = data['canal'];
-
         this.video.etiquetas = data['etiquetas'];
 
     })
